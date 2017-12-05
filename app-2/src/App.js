39e7e68f8b74c8ff93c98a3ac2ae71b82c606452
FoxChrisRealTheGit <cfox1290@gmail.com>
2017-12-05 16:07:47 -0700

    Finished app 1 & 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      arrata: ['item 1', 'item 2', 'item 3']
+    }
+  }
+
+
   render() {
+    var arrata = this.state.arrata.map((x)=>{
+      return <h2>{x}</h2>
+    })
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {arrata}
+       
       </div>
     );
   }
