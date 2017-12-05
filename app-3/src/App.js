d5144911b68b655bada055138c60867314f48d73
FoxChrisRealTheGit <cfox1290@gmail.com>
2017-12-05 16:43:20 -0700

    Finished App 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:"",
+      listStuff:['pasta', 'rice', 'bread', 'water']
+    }
+    this.handleInput = this.handleInput.bind(this)
+  }
+
+  handleInput(val){
+    this.setState({input: val})
+  }
+
   render() {
+    var displayList = this.state.listStuff.filter((x, i)=>{
+      return x.includes(this.state.input);
+    }).map((q, p)=><h2 key={p}>{q}</h2>)
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e)=>this.handleInput(e.target.value)} type="text"></input>
+       {displayList} 
       </div>
     );
   }
